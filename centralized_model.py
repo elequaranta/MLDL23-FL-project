Fixed dataset for "too many files open" error and metric computation

centralized_model.py
@@ -31,6 +31,7 @@ class CentralizedModel:
         self.serializer = serializer
 
         self.train_loader = DataLoader(train_dataset, batch_size=self.args.bs, shuffle=True, drop_last=True)
+        self.test_train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
         self.test_same_dom_loader = DataLoader(test_datasets["same_dom"], batch_size=1, shuffle=False)
         self.test_diff_dom_loader = DataLoader(test_datasets["diff_dom"], batch_size=1, shuffle=False)
 
@@ -128,7 +129,7 @@ class CentralizedModel:
         self.model.eval()
 
         if dataset_type == "train":
-            loader = self.train_loader
+            loader = self.test_train_loader
         elif dataset_type == "same_dom":
             loader = self.test_same_dom_loader
         elif dataset_type == "diff_dom":
@@ -141,7 +142,7 @@ class CentralizedModel:
                 images = images.to(self.device, dtype=torch.float32)
                 labels = labels.to(self.device, dtype=torch.long)
                 outputs = self.model(images)
-                self.update_metric(metric, outputs, labels)
+                self.update_metric(metric, outputs["out"], labels)
             results = metric.get_results()
             results["dataset"] = dataset_type
             wandb.log({"validation": results})

datasets/idda.py
@@ -21,21 +21,9 @@ class IDDADataset(VisionDataset):
         self.client_name = client_name
         self.target_transform = self.get_mapping()
 
-        self.images = []
-        self.labels = []
-
-        image_path = os.path.join(root, 'images')
-        label_path = os.path.join(root, 'labels')
-
-        for sample in self.list_samples:
-            #image_name, _ = sample.split(".")
-            sample = sample.strip()
-            image = Image.open(os.path.join(image_path, f"{sample}.jpg"), 'r')
-            label = Image.open(os.path.join(label_path, f"{sample}.png"), 'r')
-            self.images.append(image)
-            self.labels.append(label)
-            #self.labels.append(np.asarray(label, dtype=np.int32))
-
+        self.image_path = os.path.join(root, 'images')
+        self.label_path = os.path.join(root, 'labels')
+        
 
     @staticmethod
     def get_mapping():
@@ -46,8 +34,9 @@ class IDDADataset(VisionDataset):
         return lambda x: from_numpy(mapping[x])
 
     def __getitem__(self, index: int) -> Any:
-        image = self.images[index]
-        label = self.labels[index]
+        sample = self.list_samples[index].strip()
+        image = Image.open(os.path.join(self.image_path, f"{sample}.jpg"), 'r')
+        label = Image.open(os.path.join(self.label_path, f"{sample}.png"), 'r')
 
         if self.transform is not None:
             image, label = self.transform(image, label)